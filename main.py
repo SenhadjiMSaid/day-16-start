67a2a83e674aa2364e7d36186d622c58f46307e2
SenhadjiMSaid <saidsenhadji06@gmail.com>
2024-10-12 16:02:15 +0100

    Building the Coffee Machine in OOP

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from turtle import *
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-
-timmy.forward(100)
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
 
 
-my_screen = Screen()
-print(my_screen.canvwidth)
-my_screen.exitonclick()
+
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}: ")
+    if choice == "off":
+        is_on = False
+        continue
+    elif choice == "report" :
+        coffee_maker.report()
+        money_machine.report()
+        continue
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+
+
+
