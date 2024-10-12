9cca59154840346da734350324781725c05fc36e
SenhadjiMSaid <saidsenhadji06@gmail.com>
2024-10-12 14:02:08 +0100

    Turtle graphics

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+
+timmy.forward(100)
+
+
+my_screen = Screen()
+print(my_screen.canvwidth)
+my_screen.exitonclick()
